25f4609c2e0e2fe3b4ecb65e6d86662f508ec0d0
Preena27 <preenamageshwaran@gmail.com>
2026-02-20 09:32:50 +0530

    Time: 0 ms (100%), Space: 19.5 MB (16.32%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        if not s:
+            return s
+        count = 0
+        start = 0
+        res = []
+        for i in range(len(s)):
+            if s[i] == '1':
+                count += 1
+            else:
+                count -= 1               
+            if count == 0:
+                inner = self.makeLargestSpecial(s[start+1:i])
+                res.append("1" + inner + "0")
+                start = i + 1
+        res.sort(reverse=True)       
+        return "".join(res)
